d40c30e323499b879eb8d58cecaae1353d66242f
Simon Hengel <sol@typeful.net>
2026-04-13 16:49:48 +0700

    Refactoring

--- test/ResultSpec.hs
+++ test/ResultSpec.hs
@@ -11,8 +11,18 @@ spec = do
   describe "issueTitle" do
     context "with a desktop system" do
       it "creates a descriptive issue title" do
-        issueTitle 0 System.i10900K_desktop `shouldBe` "[result] 0s - desktop computer - Intel Core i9-10900K CPU"
+        issueTitle 0 System.i10900K_desktop `shouldBe`
+
+          "[result] 0s - desktop computer - Intel Core i9-10900K CPU"
 
     context "with a laptop system" do
       it "creates a descriptive issue title" do
-        issueTitle 0 System.t60_ThinkPad `shouldBe` "[result] 0s - LENOVO ThinkPad T60 - Intel Core 2 Duo Processor T7200"
+        issueTitle 0 System.dell_xps `shouldBe`
+
+          "[result] 0s - Dell Inc. XPS 13 9310 - 11th Gen Intel Core i7-1165G7"
+
+    context "with a LENOVO ThinkPad" do
+      it "creates a descriptive issue title" do
+        issueTitle 0 System.x200 `shouldBe`
+
+          "[result] 0s - LENOVO ThinkPad X200 - Intel Core2 Duo CPU     P8700 "
