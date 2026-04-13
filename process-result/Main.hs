02b491cef65f2b96502b6937f11d4eb6e2e67af4
Simon Hengel <sol@typeful.net>
2026-04-14 02:36:22 +0700

    Add `.github/workflows/process-result.yml`

--- process-result/Main.hs
+++ process-result/Main.hs
@@ -3,13 +3,38 @@ module Main (main) where
 
 import Imports
 
-import Data.Yaml (ToJSON, encodeFile)
+import Data.Ord (comparing)
+import Data.Yaml (ToJSON)
+import Data.Yaml.Pretty qualified as Yaml
+import Data.ByteString qualified as B
 import System.Environment (getArgs)
 import System.Directory (createDirectoryIfMissing)
 import System.FilePath (takeDirectory)
 
 import Result
 import SystemInfo
+
+fieldOrder :: [(Text, Int)]
+fieldOrder = flip zip [1..] [
+    "time"
+  , "concurrency"
+  , "os"
+  , "arch"
+  , "category"
+  , "chassis_type"
+  , "name"
+  , "cores"
+  , "threads"
+  , "vendor"
+  , "family"
+  , "model"
+  , "stepping"
+  , "version"
+  , "product"
+  , "board"
+  , "cpu"
+  , "ram"
+  ]
 
 instance ToJSON Result
 deriving newtype instance ToJSON Concurrency
@@ -29,5 +54,18 @@ main = do
     path :: FilePath
     path = resultPath (fromString timestamp) result.system
 
-  createDirectoryIfMissing True (takeDirectory path)
   encodeFile path result
+
+encodeFile :: FilePath -> Result -> IO ()
+encodeFile file result = do
+  ensureDirectory file
+  B.writeFile file $ Yaml.encodePretty conf result
+  where
+    conf :: Yaml.Config
+    conf = Yaml.setConfCompare (comparing byFieldOrder) Yaml.defConfig
+
+    byFieldOrder :: Text -> Int
+    byFieldOrder name = fromMaybe maxBound (lookup name fieldOrder)
+
+ensureDirectory :: FilePath -> IO ()
+ensureDirectory = createDirectoryIfMissing True . takeDirectory
