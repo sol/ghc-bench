0afdcf55ee0af0c019ad824855c95d764373c97e
Simon Hengel <sol@typeful.net>
2026-04-14 20:50:28 +0700

    Use default cabal configuration

--- src/Benchmark/Util.hs
+++ src/Benchmark/Util.hs
@@ -14,11 +14,9 @@ module Benchmark.Util (
 import Prelude as Imports
 import System.FilePath as Imports ((</>))
 
-import Command (Concurrency)
-
 import GHC.Clock (getMonotonicTimeNSec)
-import System.Environment (getEnvironment)
-import System.Process.Extra
+
+import Command
 
 measure :: IO () -> IO Int
 measure action = do
@@ -27,29 +25,3 @@ measure action = do
   end <- getMonotonicTimeNSec
   let dtSeconds = fromIntegral (end - start) / 1e9 :: Double
   pure (round dtSeconds)
-
-callWith :: Env -> FilePath -> [FilePath] -> IO ()
-callWith Env{..} command args = do
-  env <- case extend of
-    [] -> return Nothing
-    values -> Just . (values ++) <$> getEnvironment
-  callCreateProcess (proc command args) {
-      cwd = case dir of
-        "" -> Nothing
-        d -> Just d
-    , env
-    }
-
-data Env = Env {
-  dir :: FilePath
-, extend :: [(FilePath, FilePath)]
-}
-
-instance Semigroup Env where
-  Env _ envl <> Env dir envr = Env dir (envl ++ envr)
-
-instance Monoid Env where
-  mempty = Env "" []
-
-chdir :: FilePath -> Env
-chdir dir = mempty { dir }
