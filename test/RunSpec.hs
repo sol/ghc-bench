6bec9cbb3d357a0d6c1a4356919f22fe8bf1b743
Simon Hengel <sol@typeful.net>
2026-04-23 17:11:21 +0700

    Don't hardcode the system temp directory to `/tmp`

--- test/RunSpec.hs
+++ test/RunSpec.hs
@@ -10,5 +10,5 @@ spec :: Spec
 spec = do
   describe "--dry-run" do
     it "prints commands" do
-      let run = Run.run ($ "<sandbox>") True [] "/path/to/ghc-9.12.4" 20
+      let run = Run.run "/tmp/ghc-bench" ($ "<sandbox>") True [] "/path/to/ghc-9.12.4" 20
       capture_ run >>= ensureFile "test/dry-run" . encodeUtf8 . pack
