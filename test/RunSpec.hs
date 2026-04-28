b276f6b380e33a695bc05efb0c48d806d6147cea
Simon Hengel <sol@typeful.net>
2026-04-28 11:42:19 +0700

    Update RunSpec.hs

--- test/RunSpec.hs
+++ test/RunSpec.hs
@@ -5,18 +5,24 @@ import System.IO.Silently
 import README (ensureFile)
 
 import Run qualified
-import Run (DryRun(..), PrintQR(..))
+import Run (DryRun(..), Prepare(..), PrintQR(..))
 
 spec :: Spec
 spec = do
   describe "--dry-run" do
     it "prints commands" do
-      let run = Run.run "~/.cache/ghc-bench" ($ "<sandbox>") DryRun [] "/path/to/ghc-9.12.4" 20
+      let run = Run.run "~/.cache/ghc-bench" ($ "<sandbox>") DryRun NoPrepare [] "/path/to/ghc-9.12.4" 20
       capture_ run >>= ensureFile "test/dry-run" . encodeUtf8 . pack
 
   describe "parseOptions" do
     it "accepts --dry-run" do
-      Run.parseOptions ["--dry-run", "foo", "bar"] `shouldBe` (DryRun, (NoPrintQR, ["foo", "bar"]))
+      Run.parseOptions ["--dry-run", "foo", "bar"]
+        `shouldBe` (DryRun, (NoPrepare, (NoPrintQR, ["foo", "bar"])))
+
+    it "accepts --prepare" do
+      Run.parseOptions ["--prepare", "foo", "bar"]
+        `shouldBe` (NoDryRun, (Prepare, (NoPrintQR, ["foo", "bar"])))
 
     it "accepts --qr" do
-      Run.parseOptions ["--qr", "foo", "bar"] `shouldBe` (NoDryRun, (PrintQR, ["foo", "bar"]))
+      Run.parseOptions ["--qr", "foo", "bar"]
+        `shouldBe` (NoDryRun, (NoPrepare, (PrintQR, ["foo", "bar"])))
